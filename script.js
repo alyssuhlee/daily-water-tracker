541c6f8c24fffe4f04ca6cd16e845e81c306f59c
Alyssa Alexandra Lee <aslee2@up.edu.ph>
2023-09-21 17:32:31 +0800

    Deleted unnecessary code

--- script.js
+++ script.js
@@ -1,9 +1,4 @@
 const smallCups = document.querySelectorAll(".cup-small");
-const liters = document.getElementById("liters");
-const percentage = document.getElementById("percentage");
-const remained = document.getElementById("remained");
-
-updateBigCup();
 
 smallCups.forEach((cup, idx) => {
   cup.addEventListener("click", () => highlightCups(idx));
